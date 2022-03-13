467c0efecab0ba07ad03027db382f1336826c073
Mezami-Aliev <101528807+Mezami-Aliev@users.noreply.github.com>
2022-03-14 00:46:20 +0300

    Add files via upload

---
+++ myapp/api/plus.php
@@ -0,0 +1,27 @@
+
+<?php
+    session_start();
+    $user = $_SESSION["user"];
+
+$x = $_REQUEST["x"];
+$y = $_REQUEST["y"];
+
+
+//Здесь имеются существенные нарушения безопасности
+//1. Слабый пароль (его нету)
+//2. Использование учетки рута (Нарушение принципа нименьших привелегий)
+//3. Секрет в коде
+
+include("../params/billing.php");
+$conn = mysqli_connect("$db_server","$db_user","$db_pwd","billing");
+date_default_timezone_set('Europe/Moscow');
+$nowTime = date("Y-m-d H:i:s");
+
+$sql = "INSERT INTO calcs(Number1, Number2, Operation, User, TimesTamp) VALUES($x, $y, 'plus', '$user', '$nowTime')";
+//echo $sql;
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
+$z = $x + $y;
+echo $z;
+
---
+++ myapp/hello.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Hello</title>
+        <link rel="stylesheet" href="css/main.css" />
+    </head>
+    <body>
+    <a href="index1.html">Главное меню</a>
+        <?php
+            $hour = date("H");
+            if ($hour < 5)
+                echo "<h1>Доброй ночи!</h1>";
+
+            if ($hour >= 5 and $hour < 12)
+                echo "<h1>Доброe утро!</h1>";
+
+            if ($hour >= 12 and $hour < 19)
+                echo "<h1>Добрый день!</h1>";
+
+            if ($hour >= 19)
+                echo "<h1>Добрый вечер!</h1>";
+
+        ?>
+    </body>
+</html>
